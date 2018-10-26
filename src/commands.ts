336399ad784d3c1bd2546bc9376cd00b052d49e6
Jeff Groom <jgroom@ciena.com>
2018-10-25 23:02:41 -0600

    parse postman file

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,7 @@
 import * as program from 'commander';
 import chalk from 'chalk';
 import * as handler from './handler';
+import * as postman from './postman';
 
 // Initialize the commander.js CLI commands
 const initializeCommands = () => {
@@ -12,6 +13,7 @@ const initializeCommands = () => {
     .option('-o, --output <type>', 'Output the test as a specific equivalent')
     .option('-n, --no-exit', `Don't exit with code 1 when a request failed`)
     .option('-b, --bulk', 'Execute Tests and directories defined in the specified yml')
+    .option('-m, --postman', 'Convert a v2.1 Postman collection to stREST')
     .action(async (dir, cmd: any) => {
       // use time to inform the user about how long the execution of the test took
       const executionStartedTime = new Date().getTime();
@@ -21,7 +23,17 @@ const initializeCommands = () => {
       }
       // workaround for --no-exit option because options with hyphens can't be read
       cmd.noExit = cmd.rawArgs.includes('-n') || cmd.rawArgs.includes('--no-exit');
-      
+
+      if(cmd.postman){
+        handler.writeMessage('Converting Postman collections to stREST')
+        console.log();
+        const exitCode: any = await postman.convert(dir);
+        // exit code does only take values between 0-255 so it's impossible to set the exit code to like 404
+        if(exitCode == 0){
+          handler.writeMessage('Postman collection converted to stREST')
+        }
+        process.exit(exitCode);
+      }
       const exitCode: any = await handler.start(dir,cmd);
 
       const executionEndedTime = new Date().getTime();
---
+++ src/postman.ts
@@ -0,0 +1,106 @@
+// import * as recursiveWalk from 'recursive-readdir';
+import * as path from 'path';
+import * as fs from 'fs';
+import chalk from 'chalk';
+// import * as yaml from 'js-yaml';
+import { writeErrorMessage } from './handler';
+import { writeMessage } from './handler';
+// var mkdirp = require('mkdirp');
+// import * as mkdirp from mkdirp;
+
+/**
+ * Convert Postman collection to stREST requests
+ * @param collection_file [optional] Target dir with .strest.yaml files in it
+ */
+export const convert = async (collection_file: string) => {
+  let isFile = false
+  let cwd_file
+  // if a custom path was defined
+  if(collection_file !== null){
+    cwd_file = path.join(process.cwd(), collection_file);
+    if(!fs.existsSync(cwd_file)){
+      writeErrorMessage(`Path ${chalk.underline(path.join(process.cwd(), collection_file))} does not exist`);
+      return 1
+    }
+    isFile = fs.statSync(cwd_file).isFile();
+  } else {
+    writeErrorMessage(`Postman collection not specified`);
+    return 1
+  }
+  // if the path is a directory, walk through it and find all test files
+  if(!isFile) {
+    writeErrorMessage(`Specified collection ${chalk.underline(path.join(process.cwd(), collection_file))} is not a file.`);
+    return 1
+  }
+  let collection
+  try{
+    collection = require(cwd_file);
+  }catch(e){
+    if (e instanceof ReferenceError) {
+      writeErrorMessage(`ReferenceError.  Is it a json file?`);
+      return 1;
+    }else{
+      throw e
+    }
+  }
+  // Light validation on the collection
+  if(collection.info.schema == "https://schema.getpostman.com/json/collection/v2.1.0/collection.json") {
+    writeMessage(`Postman schema 2.1 confirmed`);
+  } else{
+    writeErrorMessage(`Postman schema not 2.1`);
+    return 1
+  }
+  // await(exporter(collection.item))
+  let result:any[] = []
+  let cwd = path.join(process.cwd(),collection.info.name);
+  result = await(buildDirs(collection.item, cwd, result))
+  console.log(result)
+  return 0
+}
+
+
+/**
+ * Recurse Collection Items
+ * @param item
+ */
+const buildDirs = async (items: Array<any>, working_dir: any, result: any[]) => {
+  function IsJsonString(str:string) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+  }
+  for (let i of items) {
+    const new_working_dir = path.join(working_dir, i.name.replace(/[\ +\/+]/g,"_"));
+    if(i.item || i._postman_isSubFolder) {
+      result = await(buildDirs(i.item, new_working_dir, result))
+    }else{
+      let request_name = i.name.replace(/[\ +\/+]/g,"_") + ".strest.yml"
+      let request: any = {}
+      request.path = working_dir
+      request.request = request_name
+      request[request_name] = {}
+      request[request_name].url = i.request.url.raw
+      request[request_name].method = i.request.method
+      if (i.request.header){
+        for (let header of i.request.header){
+          console.log(header)
+        }
+      }
+      if (i.request.body){
+        request[request_name].data = {}
+        if(i.request.body.mode == "raw"){
+          if(IsJsonString(i.request.body.raw)){
+            request[request_name].data.json = JSON.parse(i.request.body.raw)
+          }else{
+            request[request_name].data.raw = i.request.body.raw
+          }
+        }
+      }
+      result.push(request)
+    }
+  }
+  return result
+}
